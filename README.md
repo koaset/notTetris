1f55ab87675f246b2040ef508b04fa4dc23d8140
Karl-Oskar Smed <karloskarsmed89@gmail.com>
2015-10-03 12:12:34 +0200

    Added executable folder and run info

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 Simple clone using XNA of the game Puyo Puyo created for fun.
 Created using Visual Studio 2012 and XNA 4.0.
+
+To run:
+1. Install Microsoft XNA Framework Redistributable 4.0 - http://www.microsoft.com/en-us/download/details.aspx?id=20914
+2. Run executable in bin folder
--- notTetris/notTetris/Properties/AssemblyInfo.cs
+++ notTetris/notTetris/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Not Tetris (Rework)")]
-[assembly: AssemblyProduct("Not Tetris (Rework)")]
+[assembly: AssemblyTitle("Not Tetris")]
+[assembly: AssemblyProduct("Not Tetris")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
